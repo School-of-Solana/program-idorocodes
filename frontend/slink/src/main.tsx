e7322e6577865bcf3ea4a79ba7ace9eb9d48cf44
idorocodes <idoroyen33@gmail.com>
2025-11-26 23:34:59 +0100

    finished

--- frontend/slink/src/main.tsx
+++ frontend/slink/src/main.tsx
@@ -4,8 +4,10 @@ import "./index.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import AppRoutes from "./Routes.tsx";
 import { SolanaProvider } from "./provider.tsx";
+import "buffer";
+import { Buffer } from 'buffer';
 
-
+(window as any).Buffer = Buffer;
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
        
